f976e5823e0c6c5f4e2f5cbed32243f7a7f17e95
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-14 18:21:48 +0700

    xong

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 
 export default class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { soA: '' };
+    }
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: '#DFF5C9' }}>
-
+                <TextInput 
+                    style={{ height: 30, backgroundColor: '#fff' }} 
+                    placeholder="Enter number a"
+                    onChangeText={text => this.setState({ soA: text })}
+                />
             </View>
         );
     }
