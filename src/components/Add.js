20badf967ece22bb0b5cd541d3ed234c5a8e108d
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-14 18:45:07 +0700

    styles

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const Add = (props) => (
-    <View style={{ flex: 1, backgroundColor: '#35558A' }}>
-        <Text>{ props.soA + props.soB }</Text>
+    <View style={{ flex: 1, backgroundColor: '#35558A', justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ color: 'white', fontSize: 30 }}>{ props.soA + props.soB }</Text>
     </View>
 );
 
