4ef72f6b58b9c3088165fdb9d7aad5f84891b202
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-14 18:31:43 +0700

    xong

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,16 +4,24 @@ import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 export default class Form extends Component {
     constructor(props) {
         super(props);
-        this.state = { soA: '' };
+        this.state = { soA: '', soB: '' };
     }
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: '#DFF5C9' }}>
-                <TextInput 
-                    style={{ height: 30, backgroundColor: '#fff' }} 
+                <TextInput
+                    style={{ height: 30, backgroundColor: '#fff', margin: 10 }}
                     placeholder="Enter number a"
-                    onChangeText={text => this.setState({ soA: text })}
+                    onChangeText={text => this.setState({ soA: text, soB: this.state.soB })}
                 />
+                <TextInput
+                    style={{ height: 30, backgroundColor: '#fff', margin: 10 }}
+                    placeholder="Enter number b"
+                    onChangeText={text => this.setState({ soB: text, soA: this.state.soA })}
+                />
+                <TouchableOpacity onPress={() => console.log(this.state.soA, this.state.soB)}>
+                    <Text>Show the value</Text>
+                </TouchableOpacity>
             </View>
         );
     }
