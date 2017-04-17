3ff465c0eb3f0e51243be812a8ba2fa3eb2ebefe
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-17 18:10:42 +0700

    list

--- src/com2/App.js
+++ src/com2/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { View, StatusBar } from 'react-native';
-import Note from './Note';
+import List from './List';
 
 StatusBar.setHidden(true);
 export default class App extends Component {
     render() {
         return (
             <View style={{ backgroundColor: '#FBD87A', flex: 1 }}>
-                <Note content="Xin chao cac ban" />
+                <List content="Xin chao cac ban" />
             </View>
         );
     }
--- src/com2/List.js
+++ src/com2/List.js
@@ -1,1 +1,18 @@
 //state: mang ['Lam bai tap ve nha', 'Nop bao cao cuoi thang]
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Note from './Note';
+
+export default class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { mang: ['Lam bai tap ve nha', 'Nop bao cao cuoi thang'] };
+    }
+    render() {
+        return (
+            <View>
+                <Text>List Component</Text>
+            </View>
+        );
+    }
+}
