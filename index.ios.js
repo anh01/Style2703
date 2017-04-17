3b76dcb44befe0c7d5b8f44bf1ed610ef4f3d057
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-17 17:39:50 +0700

    start

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-// import App from './src/components/App';
-import Nav from './src/components/Nav';
+import App from './src/com2/App';
+// import Nav from './src/components/Nav';
 
 export default class StyleDemo extends Component {
   render() {
-    return <Nav />;
+    return <App />;
   }
 }
 
